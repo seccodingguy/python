a15666a5c9ef536c8bd61f6e96254eec2de46d4f
Mark Wireman <mark_wireman@hotmail.com>
2020-10-01 18:36:06 -0400

    Adding GUI to the utility.

--- pdf_utilities/getpdfhighlights.py
+++ pdf_utilities/getpdfhighlights.py
@@ -1,9 +1,28 @@
-# Based on https://stackoverflow.com/a/62859169/562769
+# The fitz highlight code is
+# based on https://stackoverflow.com/a/62859169/562769
 
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
 from typing import List, Tuple
-
+import csv
 import fitz  # install with 'pip install pymupdf'
 
+root= tk.Tk()
+
+def createGUI():
+    
+    print("Inside createGUI.")
+    canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue2', relief = 'raised')
+    canvas1.pack()
+
+    label1 = tk.Label(root, text='\nPDF Highlight and\n Notes Extraction Tool', bg = 'lightsteelblue2')
+    label1.config(font=('helvetica', 14))
+    canvas1.create_window(150, 20, window=label1)
+    exitButton = tk.Button (root, text='       Exit Application     ',command=exitApplication, bg='brown', fg='white', font=('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 230, window=exitButton)
+
+    root.mainloop()
 
 def _parse_highlight(annot: fitz.Annot, wordlist: List[Tuple[float, float, float, float, str, int, int, int]]) -> str:
     points = annot.vertices
@@ -33,6 +52,7 @@ def handle_page(page):
 
 
 def main(filepath: str) -> List:
+    createGUI()
     doc = fitz.open(filepath)
 
     highlights = []
@@ -41,6 +61,13 @@ def main(filepath: str) -> List:
 
     return highlights
 
+def exitApplication():
+    MsgBox = tk.messagebox.askquestion ('Exit Application','Are you sure you want to exit the application',icon = 'warning')
+    if MsgBox == 'yes':
+       root.destroy()
+
+
 
 if __name__ == "__main__":
     print(main("C:\\Users\\mark_\\Dropbox\\Doctoral Studies\\Thesis\\Literature\\Static_Analysis\\SOSRepair-Expressive Semantic Search for Real-World Program Repair.pdf"))
+    
